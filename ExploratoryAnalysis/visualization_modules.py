c8fd2beee3774d00424cb96f5b2bce0825b82449
nicholas-rf <nicholasrozefreitas@gmail.com>
2024-02-08 18:55:12 -0800

    Renamed + updated files
    Finalized function for visualizing category popularity. Moved data processing related functions into data_processing_modules.py. Renamed eda_modules to data_visualization_modules. Localized data processing to a separate file in order to save on memory usage.

--- ExploratoryAnalysis/visualization_modules.py
+++ ExploratoryAnalysis/visualization_modules.py
@@ -126,70 +126,32 @@ def missing_news_analysis(news : pd.DataFrame):
     ## important things to do
     ## plot distributions of time and interactions to gauge popular interaction times
     ## analyze the average clickthrough rates of articles per genre (ie total reccommendations given and clickthrough rates ) 
+           
 
-def clean_impression(impression : str = 'N55689-1') -> dict:
-    """ 
-    Cleans up a user impression for its characteristics.
-
-    Args:
-        impression (str) : A users impression on a recommended article.
-    
-    Returns:
-        impression_info (dict) : A dictionary containing keys for the rating and article in the impression.
+def plot_category_popularity(category_popularity : pd.DataFrame) -> None:
     """
-    impression_info = impression.split('-')
-    return {'score':impression_info[1], 'article_ID':impression_info[0]}  
-
-def create_popularity_dfs(news_frame : pd.DataFrame, behaviors_frame : pd.DataFrame) -> (pd.DataFrame, pd.DataFrame):
-    """ 
-    Creates dataframes to get the popularity of categories and articles through a users history and clickthrough rates.
-    This is accomplished by iterating through the users in the behaviors dataframe, and incrementing dictionaries of articles and categories
-    that get placed into dataframes.
+    Plots category popularity for user impressions and history separately using an SNS FacetGrid.
     
     Args:
-        news_frame (pd.DataFrame) : The news dataframe so that the index can be set to the news ID and used when querying user history.
-        behaviors_frame (pd.DataFrame) : The behaviors dataframe so that users last interaction history can be used to understand category popularity.
+        category_popularity (pd.DataFrame) : A pandas dataframe containing the popularity of each category within impressions and user history's.
     
     Returns:
-        category_popularity (pd.DataFrame) : A dataframe with categories as columns and popularity as a row.
-        article_popularity (pd.DataFrame) : A dataframe with articles as rows and popularity as a column.
+        None
     """
+    # Melt and then sort the data.
+    category_popularity_long = category_popularity.melt(id_vars='popularity_type', value_vars=category_popularity.columns, value_name='popularity')
+    category_popularity_long_sorted = category_popularity_long.sort_values(by=['popularity_type', 'popularity'], ascending=[True, False])
     
-    # Done for getting last history of every user (can be used for popularity up to a time when training so keeping here)
-    # max_idcs = behaviors_frame.groupby('user_id')['time'].idxmax()
-    # max_behaviors = behaviors_frame.loc[max_idcs]
+    # Create a seaborn FacetGrid for the visualization
+    g = sns.FacetGrid(category_popularity_long_sorted, col='popularity_type', sharex=False, height=5, aspect=1, hue='variable')
 
-    copynews = news_frame.set_index('news_id')
-    article_popularity_impression = {article: 0 for article in news_frame['news_id']}
-    article_popularity_history = {article: 0 for article in news_frame['news_id']}
-    category_popularity_impression = {category: 0 for category in pd.unique(copynews['category'])}
-    category_popularity_history = {category: 0 for category in pd.unique(copynews['category'])}
-    for history, impressions in zip(behaviors_frame['history'], behaviors_frame['impressions']):
-        if type(history) != float:
-            for news_id in history.split():
-                category = copynews.loc[news_id]['category']
-                category_popularity_history[category] += 1
-                article_popularity_history[news_id] += 1
-                
+    # Apply a barplot to each facet and set the labels and titles
+    g.map(sns.barplot, 'popularity','variable')
+    g.set_axis_labels(x_var="Count of interactions", y_var='Categories')
+    g.set_titles("User {col_name}")
+    g.add_legend(title='Categories')
 
-
-        if type(impressions) != float:
-            for impression in impressions.split():
-                impression_info = clean_impression(impression)
-                if impression_info['score'] == '1':
-                    article_popularity_impression[impression_info['article_ID']] += 1
-                    category = copynews.loc[impression_info['article_ID']]['category']
-                    category_popularity_impression[category] += 1
-
-    # then we have a large dictionary of articles and popularities
-    
-    article_popularity_impression = list(article_popularity_impression.items())
-    article_popularity_history = list(article_popularity_history.items())
-    
-    return (pd.DataFrame(data=category_popularity_history, index=[1]),
-            pd.DataFrame(data=category_popularity_impression, index=[1]),
-            pd.DataFrame(data=article_popularity_history, columns=['article', 'popularity_history']),
-            pd.DataFrame(data=article_popularity_impression, columns=['article', 'popularity_impression']))            
+    plt.show()
 
 def check_genre_popularity():
     """
