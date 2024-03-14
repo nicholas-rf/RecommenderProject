9ac0f0a2de7126475a21eb1eb69e2ffd61ae0ef4
nicholas-rf <nroze-freitas@ucsb.edu>
2024-03-14 09:22:20 +0000

    Matrix Factorization Modelling UJpdates

--- Modelling/matrix_modules.py
+++ Modelling/matrix_modules.py
@@ -1,25 +1,33 @@
 import pandas as pd
 import numpy as np
+from tqdm import tqdm
+import sys
+sys.path.append('/home/jovyan/work/')
+import ExploratoryAnalysis.clustering_modules as cm
 
-def load_dataset(train_split = '80_20', full=False):
+def load_in_tensorflow_full():
     """
-    Loads in the full training dataset predicated upon the train test split specified. 
+    Loads in the full tensorflow compatible dataset for usage within matrix factorization or tensorflow modelling.
     """
-    
+    # Load in the complete Tensorflow compatible dataset by iterating over all the chunks.
     dataset = pd.DataFrame()
-    if not full:    
-        for i in range(2):
-            df = pd.read_csv(f"../MIND_large/{train_split}/train_chunk{i}.csv", index_col=0)
-            dataset = pd.concat([dataset, df])   
-        
-    else:
-        for i in range(2):
-            df = pd.read_csv(f"../MIND_large/{train_split}/train_chunk{i}.csv", index_col=0)
-            dataset = pd.concat([dataset, df])
-        test = pd.read_csv(f"../MIND_large/{train_split}/test.csv", index_col=0)
-        dataset = pd.concat([dataset, test])
+    for i in range(4):
+        df = pd.read_csv(f"../MIND_large/csv/tensorflow_dataset_chunk{i}.csv", index_col=0)
+        dataset = pd.concat([dataset, df])
+    return dataset
+
+def load_dataset_for_matrix():
+    """
+    Loads in the full Tensorflow compatible dataset with clustered news and users datasets. 
+    It returns all_ratings which is the tensorflow compatible dataset which is a modified version of the tensorflow
+    compatible dataset for matrix creation, the news dataset with cluster labels, and the complete users clustered dataset. 
+    """
     
-    news = pd.read_csv('../MIND_large/csv/news_cluster_labels.csv')
+    # Load in the full Tensorflow compatible dataset.
+    dataset = load_in_tensorflow_full()
+
+    # Load in the clustered news and users datasets
+    news = pd.read_csv('../MIND_large/csv/clustered_items.csv')
     all_ratings = dataset.groupby('user_id')['news_id'].apply(list).reset_index()
     scores = dataset.groupby('user_id')['score'].apply(list).reset_index()
     all_ratings['scores'] = scores['score']
@@ -28,32 +36,7 @@ def load_dataset(train_split = '80_20', full=False):
     
     return all_ratings, news, user_clustered
 
-def load_dataset_diff(train_split = '80_20', full=False):
-    """
-    Loads in the full training dataset predicated upon the train test split specified. 
-    """
-    train_dataset = pd.DataFrame()
-
-    for i in range(2):
-        df = pd.read_csv(f"../MIND_large/{train_split}/train_chunk{i}.csv", index_col=0)
-        train_dataset = pd.concat([train_dataset, df])
-    
-    test = pd.read_csv(f"../MIND_large/{train_split}/test.csv", index_col=0)
-    full_dataset = pd.concat([train_dataset, test])
-
-    train_all_ratings = train_dataset.groupby('user_id')['news_id'].apply(list).reset_index()
-    train_scores = train_dataset.groupby('user_id')['score'].apply(list).reset_index()
-    train_all_ratings['scores'] = train_scores['score']
-
-    full_all_ratings = full_dataset.groupby('user_id')['news_id'].apply(list).reset_index()
-    full_scores = full_dataset.groupby('user_id')['score'].apply(list).reset_index()
-    full_all_ratings['scores'] = full_scores['score']
-
-    # now we have train and test as splits
-    # full_all.merge(train_all, how='left', on='user_id', suffixes=('_test', '_train')).fillna(0)
-    return train_all_ratings, full_all_ratings
-
-def create_item_cluster_mat(ratings_df, news, num_users = 255990, num_clusters = 30, isALS=False):
+def create_item_cluster_mat(ratings_df, news, num_users, num_clusters, isALS=False):
     """
     Creates a user item ratings matrix R with item clusters and potentially associated hash maps for use within ALS matrix factorization. Due to the nature of ALS, the 
     item indices that a user has interacted and the user indices that have interacted with an item get used heavily so these hash maps
@@ -80,7 +63,7 @@ def create_item_cluster_mat(ratings_df, news, num_users = 255990, num_clusters =
         
     """
     # Initialize the hash map that will create the matrix as a list of np zero arrays for each cluster and the hashmap of item clusters .
-    item_clusters = {item : cluster for item, cluster in zip(news['news_id'], news['labels'])}
+    item_clusters = {item : cluster for item, cluster in zip(news['news_id'], news['cluster'])}
     matrix = {cluster : np.full(num_users, 0, dtype='int16') for cluster in range(num_clusters)} 
     
     if isALS:
@@ -89,7 +72,7 @@ def create_item_cluster_mat(ratings_df, news, num_users = 255990, num_clusters =
 
         # Initialize the user hashmap, which is used to track a user index and all item indices that the user engaged with.
         user_idx = {user_id : set() for user_id in range(num_users)}
-        
+
     # Initialize a counter to keep track of user index.
     counter = 0
 
@@ -109,11 +92,14 @@ def create_item_cluster_mat(ratings_df, news, num_users = 255990, num_clusters =
                     cluster_idx[item_clusters[news_id]].add(counter)
                     user_idx[counter].add(item_clusters[news_id])
         counter += 1
-    
-    # Return the full matrix and arrays if we are using ALS, otherwise return only the matrix
-    return np.column_stack(list(matrix.values())), cluster_idx, user_idx if isALS else np.column_stack(list(matrix.values()))
 
-def create_user_cluster_mat(ratings_df, news, user_clustered, num_user_clusters=10, isALS=False):
+    # If we are not making hash maps for ALS, then only return the matrix. Otherwise return the matrix and hash maps.
+    if not isALS:
+        return np.column_stack(list(matrix.values()))
+    else:
+        return np.column_stack(list(matrix.values())), cluster_idx, user_idx
+
+def create_user_cluster_mat(ratings_df, news, user_clustered, num_user_clusters, isALS=False):
     """
     Creates a user item ratings matrix R with user clusters and potentially associated hash maps for use within ALS matrix factorization. Due to the nature of ALS, the 
     item indices that a user has interacted and the user indices that have interacted with an item get used heavily so these hash maps
@@ -149,21 +135,21 @@ def create_user_cluster_mat(ratings_df, news, user_clustered, num_user_clusters=
 
         # Initialize the cluster idx hash map to create a hash map that can be used to check all column indicies that have an appearance in the row.
         cluster_idx = {cluster : set() for cluster in range(num_user_clusters)}
-
     
     # Iterate over the user ids, ratings and scores.
     for user, ratings, score in zip(ratings_df['user_id'], ratings_df['news_id'], ratings_df['scores']):
         
-        # Determine the users cluster and then iterate all of their ratings
+        # Determine the users cluster and then iterate all of their ratings.
         cluster = user_clusters[user]
         for index in range(len(ratings)):
             # Get the news ID and score of their rating.
             news_id = ratings[index]
             num = score[index]
 
-            # If the score is not zero, add 1 to the clusters score for that item
+            # If the score is not zero, add 1 to the clusters score for that item.
             if num != 0:
-                matrix[news_id][cluster] += 1  # Access the column of the matrix, and then find the index in that column for that users cluster, then increment by 1
+                # Access the column of the matrix, and then find the index in that column for that users cluster, then increment by 1.
+                matrix[news_id][cluster] += 1 
                 
                 # If we are using ALS, add the relevant indices to the hash map.
                 if isALS:
@@ -172,7 +158,135 @@ def create_user_cluster_mat(ratings_df, news, user_clustered, num_user_clusters=
 
                     # Add the column index to the clusters key.
                     cluster_idx[cluster].add(item_lookup[news_id])
-        
-    return np.column_stack(list(matrix.values())), item_idx,  cluster_idx if isALS else np.column_stack(list(matrix.values()))
 
-# when making the matrix we make it with all possible users in mind
+    # If we are not making hash maps for ALS, then only return the matrix. Otherwise return the matrix and hash maps.                      
+    if not isALS:
+        return np.column_stack(list(matrix.values()))
+    else:
+        return np.column_stack(list(matrix.values())), item_idx, cluster_idx
+
+def get_features(user_features, item_features, userClustering=False, umapDim=2):
+    """
+    Uses UMAP to reduce the dimension of user and item features to a dimension specified by umapDim. By reducing the dimension
+    we standardize the number of features being added to U and V.
+        
+    Args: 
+        user_features (pd.DataFrame) : The user features in a pandas dataframe.
+        item_features (pd.DataFrame) : The item features in a pandas dataframe.
+        isALS (bool) : Signifies if the process is being done for alternating least squares or not as the matrix dimensions are different for SGD and ALS.
+    
+    Returns:
+        user_features (pd.DataFrame) : The user features in a 2d numpy array as dimension reduced embeddings.
+        item_features (pd.DataFrame) : The item features in a 2d numpy array as dimension reduced embeddings.
+    """
+    
+    # If we are using clustering we need to aggregate features per cluster so they can be properly added to U and V.
+    if userClustering:
+        # Group by the cluster label, apply the sum function to get a groups features, and then drop the user_id.
+        user_features = user_features.groupby('cluster').agg(sum).drop(columns=['user_id'])  
+
+        # Drop the news_id from the item features.
+        item_features.drop(columns="news_id", inplace=True)
+
+        # Reduce the features to a consistent size such that the matrices have the same number of latent factors.
+        user_features = cm.create_UMAP_embeddings(umapDim, user_features, metric='euclidean', n_neighbors=10)
+        item_features = cm.create_UMAP_embeddings(umapDim, item_features, metric='euclidean', n_neighbors=50)
+
+    else:
+        # Group by the cluster label, apply the sum function to get a groups features, and then drop the news_id, and reduced embeddings.
+        item_features = item_features.groupby('cluster').agg(sum).drop(columns=['news_id', "reduced_embeddings_1", "reduced_embeddings_2"])
+        
+        # Drop the user id from the user features.
+        user_features.drop(columns="user_id", inplace=True)
+
+        # Reduce the features to a consistent size such that the matrices have the same number of latent factors.
+        item_features = cm.create_UMAP_embeddings(umapDim, item_features, metric='euclidean', n_neighbors=10)
+        user_features = cm.create_UMAP_embeddings(umapDim, user_features, metric='euclidean', n_neighbors=70)
+
+    return user_features, item_features
+
+def rmse(X, Y):
+    """
+    Performes the RMSE calculation on two matrices X and Y, which is used in SGD and ALS implementations for
+    creating the predicted ratings matrix.
+    """
+    mask = X != 0
+    return np.sqrt(np.nanmean((X[mask]-Y[mask])**2))
+
+def max_update(X, Y, relative=True):
+    """
+    Performs calculations to determine how large of a difference between two matrices is after an update via either ALS or SGD.    
+    parameters:
+    - X, Y: numpy arrays or vectors
+    - relative: [True] compute relative magnitudes
+    
+    returns
+    - maximum difference between X and Y (relative to Y) 
+    
+    """
+    if relative:
+        updates = np.nan_to_num((X - Y)/Y)
+    else:
+        updates = np.nan_to_num(X - Y)
+            
+    return np.linalg.norm(updates.ravel(), np.inf)
+
+def alternating_least_squares(U, V, R, user_map, item_map, max_iterations=10, lambda_reg=0.01, threshhold = 1e-3):
+    """
+    Takes in the ratings matrix, user and item matrices, and performes alternating least squares optimization for iterations
+    determined by max_iterations regularized by lambda_reg.
+
+    Args:
+        U (np.ndarray) : The k x n user feature matrix.
+        V (np.ndarray) : The k x m item feature matrix.
+        R (np.ndarray) : The ratings matrix.
+        user_map (dict) : The hash map containing user ids as keys and item indices as values, gets used to subset the ratings matrix and V.
+        item_map (dict) : The hash map containing item ids as keys and user indices as values, gets used to subset the ratings matrix and U.
+        max_iterations (int) : The number of iterations to run alternating least squares for.
+        lambda_reg (float) : The regularization term in the alternating least squares algorithm.
+
+    Returns:
+        U (np.ndarray) : The optimized user feature matrix.
+        V (np.ndarray) : The optimized item feature matrix.
+    """
+    
+    # Initialize old versions of U and V to track max updates.
+    Uold = np.zeros_like(U)
+    Vold = np.zeros_like(V)
+
+    # Initialize RMSE and Max update lists for tracking
+    track_error = [{'iteration' : 0, 'rmse' : rmse(R, U.T @ V)}]
+    track_update = [{'iteration' : 0, 'max_update' : max(max_update(U, Uold), max_update(V, Vold))}]
+
+    # Initialize k and the number of columns in each matrix
+    k, u_cols = U.shape
+    _, v_cols = V.shape
+    k_In = np.diag(np.full(k, lambda_reg))
+
+    # Start optimizing U and V
+    for iteration in tqdm(range(1, max_iterations+1), total=max_iterations, desc='Starting ALS iterations'):
+        # Fix V and optimize U 
+        Uold = U.copy()
+        Vold = V.copy()
+        for i in tqdm(range(u_cols), total=u_cols, desc='Optimizing U', leave=False):
+            # Using translator inverse here to make sure we are using small matrix
+            ratings_row = R[i, user_map[i]]
+            rated_items = V[:, user_map[i]]
+
+            # Update the ith vector of U
+            U[:, i] = np.linalg.inv((rated_items @ rated_items.T) + k_In) @ (ratings_row @ rated_items.T)
+
+        # Fix U and optimize V
+        for j in tqdm(range(v_cols), total=v_cols, desc='Optimizing V', leave=False):
+            # Get the ratings for the item 
+            ratings_row = R[item_map[j], j]
+            user_features = U[:, item_map[j]]
+
+            # Update the jth vector of V
+            V[:, j] = np.linalg.inv((user_features @ user_features.T) + k_In) @ (ratings_row @ user_features.T)
+        
+        # Calculate the error and maximum update for this iteration
+        track_error += [{'iteration' : iteration, 'rmse' : rmse(R, U.T @ V)}]
+        track_update += [{'iteration' : iteration, 'max_update' : max(max_update(U, Uold), max_update(V, Vold))}]
+
+    return U, V, track_error, track_update
